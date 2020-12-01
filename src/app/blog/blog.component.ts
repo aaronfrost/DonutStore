69db815478a9e6e57f7bbdda62fd1ee134c35399
frosty <aaronfrost@gmail.com>
2020-12-01 12:00:06 -0700

    Final

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router, ROUTES } from '@angular/router';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { map } from 'rxjs/operators';
+
+declare var ng: any;
+
+@Component({
+    selector: 'app-blog',
+    templateUrl: './blog.component.html',
+    styleUrls: ['./blog.component.scss'],
+    preserveWhitespaces: true,
+    encapsulation: ViewEncapsulation.Emulated,
+})
+export class BlogComponent implements OnInit {
+    ngOnInit() {}
+
+    blogs$ = this.scullyRoutes.available$
+        .pipe
+        // map(routes => routes.filter(r => r.title && r.published == 'published')),
+        ();
+
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        public scullyRoutes: ScullyRoutesService,
+    ) {}
+}
